7c0f135564cc57bde52ac5930bfda81541befcec
Brijesh Kaneriya <brijesh@Brijeshs-MacBook-Air.local>
2022-12-11 03:56:01 -0500

    public contact

--- src/teamproject/emergency_contact.java
+++ src/teamproject/emergency_contact.java
@@ -38,6 +38,15 @@ public class OrderItem {
     public int getQuantity() {
         return quantity;
     }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    @Override
+    public String toString(){
+    return this.product.toString();
+    }
     
     
 }
